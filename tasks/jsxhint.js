cbae4b81f013c56eb6a65e81d8cb88fb6c04e40c
Tim Ermilov <tim.ermilov@eccenca.com>
2015-03-05 11:41:54 +0100

    better jsxhint error output

--- tasks/jsxhint.js
+++ tasks/jsxhint.js
@@ -2,10 +2,12 @@
 var path = require('path');
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var replace = require('gulp-replace');
 
 var jsxhintCmd = path.join(__dirname, '..', 'node_modules', '.bin', 'jsxhint');
 
 module.exports = function(rootDir, config) {
     return gulp.src('./src/**/*.jsx', {read: false})
-        .pipe(shell(jsxhintCmd + ' <%= file.path %>'));
+        .pipe(shell(jsxhintCmd + ' <%= file.path %>'))
+        .pipe(replace(/stdout:(.+?)\n/g, '$1'));
 };
