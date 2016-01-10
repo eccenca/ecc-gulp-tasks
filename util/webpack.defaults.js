a250a1ab3ec2766a01d41e0a6f56a3609623fd2a
Lukas Eipert <lukas.eipert@eccenca.com>
2016-01-10 11:39:02 +0100

    Made eslintrc a yaml file

--- util/webpack.defaults.js
+++ util/webpack.defaults.js
@@ -33,7 +33,7 @@ var applyDefaults = function(cfg) {
             fs: 'empty',
         },
         eslint: {
-            configFile: path.join(__dirname, '..', 'rules', 'eslintrc.json'),
+            configFile: path.join(__dirname, '..', 'rules', 'eslintrc.yml'),
         },
         module: {
             preLoaders: [
