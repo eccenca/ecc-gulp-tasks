d73ae6dba722d691c7f4adbac1e2b3b7af4174d3
Lukas Eipert <lukas.eipert@eccenca.com>
2016-01-23 17:02:51 +0100

    refactor license extractor

---
+++ util/licenseData.js
@@ -0,0 +1,45 @@
+module.exports = [
+    {
+        name: 'MIT',
+        aliases: ['MIT*', 'MIT/X11', 'MIT License']
+    },
+    {
+        name: 'BSD',
+        aliases: ['BSD*', 'BSD-3-Clause', 'BSD-2-Clause']
+    },
+    {
+        name: 'Apache-2.0',
+        aliases: [
+            'Apache*',
+            'Apache v2',
+            'Apache 2.0',
+            'Apache, Version 2.0',
+            'Apache License version 2.0',
+            'Apache License, Version 2.0'
+        ]
+    },
+    {
+        name: 'ISC',
+        aliases: ['ISC*']
+    },
+    {
+        name: 'Public Domain',
+        aliases: []
+    },
+    {
+        name: 'Unlicense',
+        aliases: []
+    },
+    {
+        name: 'WTFPL',
+        aliases: []
+    },
+    {
+        name: 'CC-BY',
+        aliases: ['CC-BY-4.0', 'CC-BY-3.0']
+    },
+    {
+        name: 'CC-0',
+        aliases: ['CC0 1.0 Universal License', 'CC0–1.0', 'CC0-1.0']
+    }
+];
