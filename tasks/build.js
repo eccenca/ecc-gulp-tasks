0b03918de3fe6649e8328414f7a4cce369b376f8
Lukas Eipert <lukas.eipert@eccenca.com>
2016-01-27 16:17:12 +0100

    require style.css in component es5 files.

--- tasks/build.js
+++ tasks/build.js
@@ -1,8 +1,13 @@
 /* eslint no-var: 0 */
+
+var fs = require('fs');
+var path = require('path');
+
 var webpack = require('webpack');
 var definePlugin = require('../util/definePlugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpackBuildCB = require('../util/webpackBuildCB');
+var SCSSBannerPlugin = require('../util/SCSSBannerPlugin');
 
 module.exports = function(config, callback) {
 
@@ -13,6 +18,19 @@ module.exports = function(config, callback) {
         definePlugin,
         new ExtractTextPlugin('style.css'),
     ];
+
+    var styleSCSS = path.join(config.webpackConfig.production.context, 'style', 'style.scss');
+
+    if(fs.existsSync(styleSCSS)){
+
+        var outputPath = config.webpackConfig.production.output.path;
+        var outputFileName = config.webpackConfig.production.output.filename;
+
+        optimizations.push(
+            new SCSSBannerPlugin(outputPath, outputFileName, styleSCSS)
+        )
+    }
+
     if (wpConfig.plugins) {
         wpConfig.plugins = wpConfig.plugins.concat(optimizations);
     } else {
