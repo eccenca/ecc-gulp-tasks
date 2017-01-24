86764420d7269856f45e2aba105a314a1137c073
Lukas Eipert <lukas.eipert@eccenca.com>
2017-01-24 23:05:49 +0100

    add gulp task to update readme depending on env.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var mocha = require('gulp-spawn-mocha');
 
 var lint = require('./src/tasks/lint');
 
+var _ = require('lodash');
+var fs = require('fs-extra');
+
 gulp.task('test', function() {
     return gulp.src(['test/*.test.js'], {read: false})
         .pipe(mocha({
@@ -13,4 +16,16 @@ gulp.task('test', function() {
 
 gulp.task('lint', function() {
     return lint({lintingFiles: './src/tasks/*.js'});
+});
+
+
+gulp.task('updateREADME', function() {
+
+    var README = fs.readFileSync('./README.md', 'utf8');
+    var envTemplate = _.template(fs.readFileSync('./ENV.md.template', 'utf8'));
+    var versions = require('./env.json');
+    README = README.replace(/<!-- ENV -->[\S\s]+<!-- ENV:END -->\n/igm, envTemplate(versions));
+
+    fs.writeFileSync('./README.md', README);
+
 });
