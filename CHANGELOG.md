8832060a15096c1397d5973207e73921276ff8cc
Lukas Eipert <lukas.eipert@eccenca.com>
2017-02-15 14:08:24 +0100

    add missing __BRANCH__ variable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,8 +3,8 @@ All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/) and [Keep A Changelog's Format](http://keepachangelog.com/).
 
 ## [Unreleased]
-
-TODO: add at least one Added, Changed, Deprecated, Removed, Fixed or Security section
+### Fixed
+- Added missing `__BRANCH__` variable
 
 ## [3.3.1] 2017-02-09 
 ### Fixed
--- src/webpack/plugins/definePlugin.js
+++ src/webpack/plugins/definePlugin.js
@@ -1,5 +1,20 @@
 const webpack = require('webpack');
 
+let branch = 'BRANCH';
+
+if (process.env.GT_BUILD_BRANCH) {
+    branch = process.env.GT_BUILD_BRANCH;
+    console.log('BRANCH: ' + branch);
+} else {
+    try {
+        const execSync = require('child_process').execSync;
+        branch = execSync('git rev-parse --abbrev-ref HEAD').toString();
+        console.log('BRANCH: ' + branch);
+    } catch (e) {
+        // we die gracefully, as git errored, or something
+    }
+}
+
 let version = 'VERSION';
 
 if (process.env.GT_BUILD_VERSION) {
@@ -15,12 +30,12 @@ if (process.env.GT_BUILD_VERSION) {
     }
 }
 
-
 // definePlugin takes raw strings and inserts them, so you can put strings of JS if you want.
 const definePlugin = new webpack.DefinePlugin({
     __WEBPACK__: true, // say we're the webpack
     __DEV__: process.env.BUILD_DEV, // dev environment indication
     __VERSION__: JSON.stringify(version),
+    __BRANCH__: JSON.stringify(branch),
 });
 
 module.exports = definePlugin;
