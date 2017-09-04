3efdcd2ef58a3d66ff813c5f01b76e668199b112
Kay Vollers <kay.vollers@eccenca.com>
2017-09-04 14:03:01 +0200

    fix typo

--- src/tasks/docs-channels.js
+++ src/tasks/docs-channels.js
@@ -2,20 +2,16 @@ const jsdoc2md = require('jsdoc-to-markdown');
 const fs = require('fs');
 
 // define which parts of whole jsdoc2md docu should be rendered
-const docStyle = (
-    `## Channels
-{{#ChannelRender functions}}{{this}}{{/ChannelRender}}`
-);
+const docStyle = `## Channels
+{{#ChannelRender functions}}{{this}}{{/ChannelRender}}`;
 
 module.exports = function(config) {
     const glob = config.docPath || 'src/**/*.{js,jsx}';
     const helper = 'src/util/ChannelRender.js';
-    const output = (
-        jsdoc2md.renderSync({
-            files: glob,
-            template: docStyle,
-            helper,
-        })
-    );
+    const output = jsdoc2md.renderSync({
+        files: glob,
+        template: docStyle,
+        helper,
+    });
     fs.writeFile('.tmp/Store.md', output);
 };
