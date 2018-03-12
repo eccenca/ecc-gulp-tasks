b956fceaedd514315a674f7aa572a13a9f46f30f
Kay Vollers <kay.vollers@eccenca.com>
2018-03-12 14:40:58 +0100

    use displayName as component name

--- test/fixtures/docs/Component.jsx
+++ test/fixtures/docs/Component.jsx
@@ -4,6 +4,7 @@ var React = require('react');
  * General component description.
  */
 var Component = React.createClass({
+    displayName: 'Component',
     propTypes: {
         /**
          * Description of prop "foo".
